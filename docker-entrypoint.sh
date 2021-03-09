fbc8839196958a155de318b6b02ae08b34343d5e
Alexander Neumann <neumann@dbis.rwth-aachen.de>
2021-03-09 20:58:47 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 sed -i "s|http://localhost:9011|${BOOTSTRAP}|" /app/src/environments/environment.prod.ts
 sed -i "s|localhost:8089|${Y_WEBSOCKET}|" /app/src/environments/environment.prod.ts
 sed -i "s|f8622260-875b-499a-82db-db55f89f9deb|${OIDC_CLIENT_ID}|" /app/src/environments/environment.prod.ts
-node --max_old_space_size=8000 node_modules/@angular/cli/bin/ng serve --host 0.0.0.0 --prod=true --disable-host-check 
+node --max_old_space_size=8000 node_modules/@angular/cli/bin/ng serve --host 0.0.0.0 --prod=true --disable-host-check  --serve-path ${PATH}
