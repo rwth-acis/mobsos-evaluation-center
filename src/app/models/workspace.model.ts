f63d00054cc9c3308269cc937f3fdfc83fc64bf8
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-06-09 15:46:24 +0200

    comments

--- src/app/models/workspace.model.ts
+++ src/app/models/workspace.model.ts
@@ -5,7 +5,7 @@ import { Visitor } from './user.model';
 export interface ApplicationWorkspace {
   // This is the workspace for a given application. It is shared with other members of the community and is unique for each service
   createdBy: string; // original author
-  visitors: Visitor[];
+  visitors: Visitor[]; // visitors of the room
   model: SuccessModel;
 }
 export interface ServiceWorkspaces {
@@ -13,7 +13,7 @@ export interface ServiceWorkspaces {
   [serviceName: string]: ApplicationWorkspace;
 }
 export interface CommunityWorkspace {
-  members: string[]; // username of members
-  catalog: MeasureCatalog;
+  members?: string[]; // username of members
+  catalog: MeasureCatalog; // measure Catalog is shared by the whole community
   workspaces: ServiceWorkspaces;
 }
