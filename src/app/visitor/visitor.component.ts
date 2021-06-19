5b95c3b0c7612455d295b435624d0c68d80cca2f
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-06-19 02:10:35 +0200

    visitor componentt

--- src/app/visitor/visitor.component.ts
+++ src/app/visitor/visitor.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map, withLatestFrom } from 'rxjs/operators';
+import {
+  ASSETS_LOADED,
+  DIMENSIONS_IN_MODEL,
+  MEASURE_CATALOG,
+  SELECTED_GROUP_ID,
+  SELECTED_SERVICE_NAME,
+  SUCCESS_MODEL,
+} from '../services/store.selectors';
+import { iconMap, translationMap } from '../success-modeling/config';
 
 @Component({
   selector: 'app-visitor',
   templateUrl: './visitor.component.html',
-  styleUrls: ['./visitor.component.scss']
+  styleUrls: ['./visitor.component.scss'],
 })
 export class VisitorComponent implements OnInit {
+  selectedServiceName$ = this.ngrxStore.select(SELECTED_SERVICE_NAME);
+  selectedGroupId$ = this.ngrxStore.select(SELECTED_GROUP_ID);
+  assetsLoaded$ = this.ngrxStore.select(ASSETS_LOADED);
+  showSuccessModelEmpty$ = this.ngrxStore
+    .select(DIMENSIONS_IN_MODEL)
+    .pipe(
+      map(
+        (dimensions) =>
+          dimensions.find((dimension) => dimension.length > 0) ===
+          undefined,
+      ),
+    );
+  successMode$ = this.ngrxStore.select(SUCCESS_MODEL);
+  measureCatalog$ = this.ngrxStore.select(MEASURE_CATALOG);
 
-  constructor() { }
+  translationMap = translationMap; // maps dimensions to their translation keys
+  iconMap = iconMap; // maps dimensions to their icons
 
-  ngOnInit(): void {
-  }
+  constructor(private ngrxStore: Store) {}
 
+  ngOnInit(): void {}
 }
