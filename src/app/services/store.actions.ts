2fd330f4577aeec7cafb7c31ad104edd45d4d63d
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-07-25 19:04:04 +0200

    add actions to fetch and store questionnaires

--- src/app/services/store.actions.ts
+++ src/app/services/store.actions.ts
@@ -2,6 +2,7 @@ import { createAction, props } from '@ngrx/store';
 import { GroupInformation } from '../models/community.model';
 
 import { Measure } from '../models/measure.model';
+import { Questionnaire } from '../models/questionnaire.model';
 import { ReqbazProject, Requirement } from '../models/reqbaz.model';
 import { ServiceInformation } from '../models/service.model';
 import { SuccessFactor } from '../models/success.model';
@@ -15,6 +16,7 @@ export enum HttpActions {
   FETCH_MEASURE_CATALOG_FOR_GROUP = 'fetch measure catalog for current Group',
   FETCH_SUCCESS_MODEL_FOR_GROUP_AND_SERVICE = 'fetch success model for current Group and current service',
   FETCH_VISUALIZATION_DATA = 'fetch visualization data from the qvs for a given sql query',
+  FETCH_QUESTIONNAIRES = 'fetch questionnaires from the mobsos surveys',
   SAVE_MODEL_AND_CATALOG = 'send an update to the server for both model and catalog',
   SAVE_CATALOG = 'save catalog on the server',
   SAVE_MODEL = 'save model on the server',
@@ -49,6 +51,7 @@ export enum StoreActions {
   REMOVE_REQUIREMENTS_BAZAR_PROJECT = 'remove a requirement bazar project from the success model',
   STORE_REQUIREMENTS = 'Store the requirements for the current req bazar project',
   SET_NUMBER_OF_REQUIREMENTS = 'set the number of requirements for the current project',
+  STORE_QUESTIONNAIRES = 'store the fetched questionnaires in store',
 }
 
 export enum StateActions {
@@ -64,7 +67,6 @@ export enum StateActions {
   DECREMENT_LOADING = 'Decrease the number of current http calls',
   TOGGLE_EXPERT_MODE = 'Toggle the expert mode for raw edit of success model and measure catalog',
   INITIALIZE_STATE = 'Initializes the state of the application. This action should only be called once.',
-
   SUCCESS_RESPONSE = 'response was successfully',
   FAILURE_RESPONSE = 'response was not successfully',
 }
@@ -85,7 +87,16 @@ export const fetchSuccessModel = createAction(
   props<{ groupId; serviceName }>(),
 );
 
+export const fetchQuestionnaires = createAction(
+  HttpActions.FETCH_QUESTIONNAIRES,
+);
+
 // storing
+export const storeQuestionnaires = createAction(
+  StoreActions.STORE_QUESTIONNAIRES,
+  props<{ questionnaires: Questionnaire[] }>(),
+);
+
 export const storeServices = createAction(
   StoreActions.STORE_SERVICES,
   props<{ servicesFromL2P; servicesFromMobSOS }>(),
