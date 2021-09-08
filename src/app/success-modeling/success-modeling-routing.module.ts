ad51402bea764c7f83d4bc2fe611f1bfdabfa12e
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-09-08 17:11:34 +0200

    remove unused import

--- src/app/success-modeling/success-modeling-routing.module.ts
+++ src/app/success-modeling/success-modeling-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CustomersComponent } from './customers.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
 
 const routes: Routes = [
