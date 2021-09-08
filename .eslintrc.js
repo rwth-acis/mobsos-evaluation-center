7e626cd903f0b5973369db08af903a5b9d2a3c41
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-09-08 19:08:21 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,9 @@ module.exports = {
       rules: {
         quotes: [2, 'single', { avoidEscape: true }],
         'no-underscore-dangle': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
       },
     },
   ],
