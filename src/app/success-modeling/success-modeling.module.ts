644717d6f1e6549fb62de7109769899badc99347
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-10-05 21:01:23 +0200

    remove unused dep

--- src/app/success-modeling/success-modeling.module.ts
+++ src/app/success-modeling/success-modeling.module.ts
@@ -25,7 +25,6 @@ import { PickMeasureDialogComponent } from './success-model/success-dimension/su
 import { QuestionnairesComponent } from './success-model/questionnaires/questionnaires.component';
 import { PickQuestionnaireDialogComponent } from './success-model/questionnaires/pick-questionnaire-dialog/pick-questionnaire-dialog.component';
 import { DeleteQuestionnaireDialogComponent } from './success-model/questionnaires/delete-questionnaire-dialog/delete-questionnaire-dialog.component';
-import { SqlTableComponent } from '../shared/sql-table/sql-table.component';
 import { VisualizationInfoComponent } from './success-model/success-dimension/success-factor/success-measure/visualizations/visualization-info/visualization-info.component';
 import { RawDataDialogComponent } from './success-model/success-dimension/success-factor/success-measure/visualizations/raw-data-dialog/raw-data-dialog.component';
 import { PickReqbazProjectComponent } from './requirements/pick-reqbaz-project/pick-reqbaz-project.component';
