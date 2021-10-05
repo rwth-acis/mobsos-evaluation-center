e77a09bff10cc124a76906f27cc1ee4e5c28a5e7
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-10-05 20:07:51 +0200

    adjust initial query

--- src/app/query-visualization/query-visualization/query-visualization.component.ts
+++ src/app/query-visualization/query-visualization/query-visualization.component.ts
@@ -8,20 +8,23 @@ import { startWith } from 'rxjs/operators';
   styleUrls: ['./query-visualization.component.scss'],
 })
 export class QueryVisualizationComponent implements OnInit {
+  static initialValue = 'SELECT ID, REMARKS FROM MESSAGE limit 10';
   visualizationChoices = {
     Value: 'success-modeling.edit-measure-dialog.choice-value',
     Chart: 'success-modeling.edit-measure-dialog.choice-chart',
     Table: 'query-visualization.choice-table',
   };
 
-  form = this.fb.group({
-    query: ['SELECT * FROM MESSAGE limit 1'],
-    updateOn: 'blur',
-  });
+  form = this.fb.group(
+    { query: [QueryVisualizationComponent.initialValue] },
+    { updateOn: 'blur' },
+  );
 
   queryInput$ = this.form
     .get('query')
-    .valueChanges.pipe(startWith('SELECT * FROM MESSAGE limit 1'));
+    .valueChanges.pipe(
+      startWith(QueryVisualizationComponent.initialValue),
+    );
 
   constructor(private fb: FormBuilder) {}
 
