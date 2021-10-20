920b47e7d54a4b2a46669dcd9464b8acf940f609
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-10-20 17:43:04 +0200

    fix # 37

--- src/app/services/store.reducer.ts
+++ src/app/services/store.reducer.ts
@@ -502,7 +502,7 @@ function mergeGroupData(
   groupsFromContactService,
   groupsFromMobSOS?,
 ) {
-  groups = cloneDeep(groups);
+  groups = cloneDeep(groups) ||{};
 
   // mark all these groups as groups the current user is a member of
   if (groupsFromContactService) {
