c6433ce7010b69da29942bf5e63905ae9bcacf5c
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-02-11 18:07:00 +0100

    measures can now be imported

--- src/app/shared/dialogs/import-dialog/import-dialog.component.ts
+++ src/app/shared/dialogs/import-dialog/import-dialog.component.ts
@@ -1,4 +1,19 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { SuccessModel } from 'src/app/models/success.model';
+import {
+  enableEdit,
+  setService,
+  setServiceName,
+  storeCatalog,
+  storeSuccessModel,
+} from 'src/app/services/store.actions';
 
 @Component({
   selector: 'app-import-dialog',
@@ -7,52 +22,55 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class ImportDialogComponent implements OnInit {
   @HostListener('window:drop', ['$event.target'])
-  onClick(e) {
-    this.dropHandler(e);
-    e.preventDefault();
-  }
-  constructor() {
-    window.addEventListener(
-      'dragover',
-      function (e: Event) {
-        e = e || event;
-        e.preventDefault();
-      },
-      false,
-    );
-    window.addEventListener(
-      'drop',
-      function (e: Event) {
-        e = e || event;
-        e.preventDefault();
-      },
-      false,
-    );
-  }
+  fileName: string;
+  static xml: string;
+  constructor(
+    private store: Store,
+    private dialogRef: MatDialogRef<ImportDialogComponent>,
+  ) {}
 
   ngOnInit(): void {}
   dropHandler(ev) {
-    console.log('File(s) dropped');
+    const file = ev.target.files[0];
+    this.fileName = file.name;
+    if (this.fileName) {
+      var reader = new FileReader();
+      reader.readAsText(file, 'UTF-8');
+      reader.onload = function (evt) {
+        ImportDialogComponent.xml = evt.target.result as string;
+      };
+      reader.onerror = function (evt) {};
+    }
+  }
 
-    if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-        // If dropped items aren't files, reject them
-        if (ev.dataTransfer.items[i].kind === 'file') {
-          var file = ev.dataTransfer.items[i].getAsFile();
-          console.log('... file[' + i + '].name = ' + file.name);
-        }
-      }
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-        console.log(
-          '... file[' +
-            i +
-            '].name = ' +
-            ev.dataTransfer.files[i].name,
-        );
-      }
+  importXML() {
+    const parser = new DOMParser();
+    const XMLElement = parser.parseFromString(
+      ImportDialogComponent.xml,
+      'text/xml',
+    );
+    const isSuccessModel = ImportDialogComponent.xml
+      .slice(0, 10)
+      .includes('Success');
+    const isMeasureCatalog = ImportDialogComponent.xml
+      .slice(0, 10)
+      .includes('Catalog');
+
+    if (isSuccessModel) {
+      const model = SuccessModel.fromXml(XMLElement.documentElement);
+      this.store.dispatch(
+        setServiceName({ serviceName: model.service }),
+      );
+      this.store.dispatch(
+        storeSuccessModel({ xml: ImportDialogComponent.xml }),
+      );
+      this.store.dispatch(enableEdit());
+      this.dialogRef.close();
+    } else if (isMeasureCatalog) {
+      this.store.dispatch(
+        storeCatalog({ xml: ImportDialogComponent.xml }),
+      );
+      this.dialogRef.close();
     }
   }
 }
