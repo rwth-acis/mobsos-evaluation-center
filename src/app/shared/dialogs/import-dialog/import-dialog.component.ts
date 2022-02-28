bd9bb0ac80d054ca20b54eed0c8605624b5bf03a
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-02-28 11:06:59 +0100

    make checks more robust

--- src/app/shared/dialogs/import-dialog/import-dialog.component.ts
+++ src/app/shared/dialogs/import-dialog/import-dialog.component.ts
@@ -57,12 +57,9 @@ export class ImportDialogComponent implements OnInit {
       ImportDialogComponent.xml,
       'text/xml',
     );
-    const isSuccessModel = ImportDialogComponent.xml
-      .slice(0, 10)
-      .includes('Success');
-    const isMeasureCatalog = ImportDialogComponent.xml
-      .slice(0, 10)
-      .includes('Catalog');
+    const rootNodeName = XMLElement.firstChild.nodeName;
+    const isSuccessModel = rootNodeName === 'SuccessModel';
+    const isMeasureCatalog = rootNodeName === 'MeasureCatalog';
     if (!isSuccessModel && !isMeasureCatalog)
       return alert(
         'The filetype is not supported. Please only submit valid success model or catalog files',
