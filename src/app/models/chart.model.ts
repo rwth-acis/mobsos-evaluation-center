f2e6cde6862391db13d966dd6cd56b266229f2c8
lakhoune <a.b.lakhoune@gmail.com>
2022-03-11 09:47:39 +0100

    rename data attribute

--- src/app/models/chart.model.ts
+++ src/app/models/chart.model.ts
@@ -1,12 +1,23 @@
 import { Formatter } from 'angular-google-charts';
-
-export class ChartData {
+/**
+ * Object which contains the data required as input for google charts
+ */
+export class ChartData implements ChartData {
   constructor(
     public title: string,
     public chartType: string,
     public data: any[][],
-    public columns: string[],
-    public options: Record<string, unknown>,
+    public columnNames: string[],
+    public options: object,
     public formatters?: Formatter[],
   ) {}
+}
+
+export interface ChartData {
+  title: string; // Title of the chart
+  chartType: string; // Type of chart
+  data: any[][]; // Data for the chart
+  options: object; // Options for the chart
+  columnNames: string[]; // Column names for the chart
+  formatters?: Formatter[]; // Formats for the columns
 }
