7755d624f14c99f9ca535348b1ed68056257053f
lakhoune <a.b.lakhoune@gmail.com>
2022-03-16 07:12:07 +0100

    export config

--- src/app/components/oidc/oidc-signin/oidc-signin.component.ts
+++ src/app/components/oidc/oidc-signin/oidc-signin.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { storeUser } from 'src/app/services/store/store.actions';
 import { User } from 'src/app/models/user.model';
+import { userManagerSettings } from './user.manager.settings';
 
 @Component({
   selector: 'app-oidc-signin',
@@ -12,10 +13,15 @@ import { User } from 'src/app/models/user.model';
 })
 export class OidcSigninComponent implements OnInit {
   constructor(private router: Router, private ngrxStore: Store) {
-    void new UserManager({}).signinRedirectCallback().then((user) => {
-      this.ngrxStore.dispatch(storeUser({ user: user as User }));
-      void this.router.navigateByUrl('/');
-    });
+    void new UserManager(userManagerSettings)
+      .signinRedirectCallback()
+      .then((user) => {
+        this.ngrxStore.dispatch(storeUser({ user: user as User }));
+        void this.router.navigateByUrl('/');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   ngOnInit(): void {}
