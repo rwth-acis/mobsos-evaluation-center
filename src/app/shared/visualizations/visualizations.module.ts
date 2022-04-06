1bcc682a9db2e40dde762143fe57743dec2f7081
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-04-06 16:51:20 +0200

    fix import

--- src/app/shared/visualizations/visualizations.module.ts
+++ src/app/shared/visualizations/visualizations.module.ts
@@ -14,6 +14,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { MatIconModule } from '@angular/material/icon';
     TranslateModule,
     MatPaginatorModule,
     MatButtonModule,
+    MatTableModule,
     MatIconModule,
   ],
   exports: [VisualizationComponent],
