02377d35503d5bae99cad68d6943166f18f7120a
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-04-22 11:39:01 +0200

    latex pipe

---
+++ src/app/shared/latex.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { parse } from 'mathjs';
+import katex from 'katex';
+@Pipe({
+  name: 'latex',
+})
+export class LatexPipe implements PipeTransform {
+  /**
+   * Renders a math expression into a latex html string
+   * @param expression
+   * @returns
+   */
+  transform(expression: string): unknown {
+    const tex = parse(expression).toTex();
+    return `\\( ${tex} \\)`;
+    return katex.renderToString(tex); //fractions are not displayed correctly
+  }
+}
