5dab4ccef3353686373c438d7378f6682391f859
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-05-20 12:04:47 +0200

    fix if check

--- src/app/components/success-modeling/success-model/surveys/pick-questionnaire-dialog/pick-questionnaire-dialog.component.ts
+++ src/app/components/success-modeling/success-model/surveys/pick-questionnaire-dialog/pick-questionnaire-dialog.component.ts
@@ -33,7 +33,7 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./pick-questionnaire-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PickQuestionnaireDialogComponent implements OnInit {
+export class PickQuestionnaireDialogComponent {
   selectedQuestionnaire: Questionnaire;
   addMeasures = true;
   assignMeasures = true;
@@ -74,8 +74,6 @@ export class PickQuestionnaireDialogComponent implements OnInit {
   //   }
   // }
 
-  ngOnInit(): void {}
-
   async fetchForm(id: number) {
     this.ngrxStore.dispatch(
       fetchQuestionnaireForm({
@@ -97,13 +95,13 @@ export class PickQuestionnaireDialogComponent implements OnInit {
         }),
       ),
     );
-
-    if (result instanceof HttpErrorResponse) {
+    if ('formXML' in result) {
+      const xml = (result as { formXML: string }).formXML;
+      return xml;
+    } else if (result instanceof HttpErrorResponse) {
       throw result;
     }
-
-    const xml = (result as { formXML: string }).formXML;
-    return xml;
+    return null;
   }
 
   async onSubmit() {
