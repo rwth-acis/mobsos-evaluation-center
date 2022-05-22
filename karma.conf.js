f96f29680715f0fe3e799c18547bb126846a6be0
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-05-22 22:30:54 +0200

    debug

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     ChromeHeadlessCI: {
       base: 'ChromeHeadless',
       flags: ['--no-sandbox'],
