25f59c651174df32d0bd859f870971e89af7ac88
lakhoune <a.b.lakhoune@gmail.com>
2022-05-31 16:19:33 +0200

    fix observable for late subscribers

--- src/app/shared/sql-table/sql-table.component.ts
+++ src/app/shared/sql-table/sql-table.component.ts
@@ -14,6 +14,7 @@ import {
   distinctUntilChanged,
   filter,
   map,
+  shareReplay,
   startWith,
   switchMap,
   tap,
@@ -92,6 +93,7 @@ export class SqlTableComponent
         ),
       ),
       distinctUntilChanged(),
+      shareReplay(1),
     );
 
     this.vdata$ = storeData$.pipe(map((vdata) => vdata?.data));
