b6d0d27336d98526160ef840fb1d646dac068f0d
lakhoune <a.b.lakhoune@gmail.com>
2022-06-10 21:24:49 +0200

    add search for surveys

--- src/app/components/success-modeling/success-model/surveys/pick-survey-dialog/pick-survey-dialog.component.ts
+++ src/app/components/success-modeling/success-model/surveys/pick-survey-dialog/pick-survey-dialog.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipEvent } from '@angular/material/chips';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { firstValueFrom, map, take } from 'rxjs';
+import {
+  combineLatest,
+  firstValueFrom,
+  map,
+  Observable,
+  take,
+} from 'rxjs';
 import { Questionnaire } from 'src/app/models/questionnaire.model';
-import { Survey } from 'src/app/models/survey.model';
+import { ISurvey, Survey } from 'src/app/models/survey.model';
 import {
   joinAbsoluteUrlPath,
   Las2peerService,
@@ -30,11 +44,21 @@ interface PickQuestionnaireResult {
   templateUrl: './pick-survey-dialog.component.html',
   styleUrls: ['./pick-survey-dialog.component.scss'],
 })
-export class PickSurveyDialogComponent {
-  selectedSurvey: Survey;
+export class PickSurveyDialogComponent implements OnInit {
+  @ViewChild('inputRef', { static: true })
+  inputRef: ElementRef<HTMLInputElement>;
+
   addMeasures = true;
   assignMeasures = true;
   mobsosSurveysUrl = environment.mobsosSurveysUrl;
+  filteredOptions$: Observable<
+    { label: string; surveys: Survey[] }[]
+  >;
+
+  get selectedSurvey() {
+    return this.form.get('selectedSurvey').value;
+  }
+
   surveys$ = this.ngrxStore.select(SURVEYS_NOT_IN_MODEL);
   currentSurveys$ = this.surveys$.pipe(
     map((surveys) =>
@@ -50,20 +74,75 @@ export class PickSurveyDialogComponent {
   );
   constructor(
     private dialogRef: MatDialogRef<PickSurveyDialogComponent>,
-    private ngrxStore: Store,
     private dialog: MatDialog,
     private l2p: Las2peerService,
+    private ngrxStore: Store,
+    private fb: FormBuilder,
   ) {}
+
+  form = this.fb.group({
+    searchInput: [''],
+    selectedSurvey: [null],
+    addMeasures: [true],
+    assignMeasures: [true],
+  });
+
+  ngOnInit() {
+    this.filteredOptions$ = combineLatest([
+      this.surveys$,
+      this.form.get('searchInput').valueChanges,
+    ]).pipe(
+      map(([surveys, input]) => {
+        const filteredSurveys = surveys.filter((survey) => {
+          return survey.name
+            ?.toLowerCase()
+            .includes(input?.toLowerCase());
+        });
+
+        return filteredSurveys.reduce(
+          (acc, curr) => {
+            if (curr.end > new Date()) {
+              acc[0].surveys.push(curr);
+            } else {
+              acc[1].surveys.push(curr);
+            }
+            return acc;
+          },
+          [
+            { label: 'Current Surveys', surveys: [] },
+            { label: 'Past Surveys', surveys: [] },
+          ],
+        );
+      }),
+    );
+  }
+
+  async setSelectedSurvey(event: MatAutocompleteSelectedEvent) {
+    const surveys = await firstValueFrom(this.surveys$.pipe(take(1)));
+    const selectedSurvey = surveys.find(
+      (survey) => survey.name === event.option.value,
+    );
+    if (selectedSurvey) {
+      this.form.get('selectedSurvey').setValue(selectedSurvey);
+      this.form.get('searchInput').setValue('');
+      this.inputRef.nativeElement.value = ''; // needed to reset the input (https://github.com/angular/components/issues/10968#issuecomment-545963282)
+      this.form.get('searchInput').disable();
+    }
+  }
+
+  remove(event: MatChipEvent) {
+    this.form.get('selectedSurvey').setValue(null);
+    this.form.get('searchInput').enable();
+  }
 
   onAddMeasuresChange(addMeasures: boolean): void {
     if (!addMeasures) {
-      this.assignMeasures = false;
+      this.form.get('assignMeasures').setValue(false);
     }
-    this.addMeasures = addMeasures;
   }
   closeDialog() {
     this.dialogRef.close({
-      selectedSurvey: this.selectedSurvey,
+      selectedSurvey: this.form.get('selectedSurvey').value,
       addMeasures: this.addMeasures,
       assignMeasures: this.assignMeasures,
     });
